b12615dd119801eec9b444496c3d54ee4d2111f1
diprasetyamr <diprasetya.mr@mailbox.org>
2018-08-05 13:14:55 +0200

    add 2 publisher and 2 subscriber

--- routes/rosnode.js
+++ routes/rosnode.js
@@ -6,20 +6,36 @@ const std_msgs = rosnodejs.require('std_msgs').msg;
 
 function talker() {
   // Register node with ROS master
-  rosnodejs.initNode('/talker_node')
+  rosnodejs.initNode('/nodejs_node')
     .then((rosNode) => {
       // Create ROS publisher on the 'chatter' topic with String message
       let pub = rosNode.advertise('/chatter', std_msgs.String);
+      let pub2 = rosNode.advertise('/chatter2', std_msgs.String);
       let count = 0;
       const msg = new std_msgs.String();
+      const msg2 = new std_msgs.String();
+
+      let sub = rosNode.subscribe('/chatter', std_msgs.String,
+        (data) => { // define callback execution
+          rosnodejs.log.info('I heard: [' + data.data + ']');
+        }
+      );
+
+      let sub2 = rosNode.subscribe('/chatter2', std_msgs.String,
+        (data) => { // define callback execution
+          rosnodejs.log.info('I heard2: [' + data.data + ']');
+        }
+      );
       // Define a function to execute every 100ms
       setInterval(() => {
         // Construct the message
         msg.data = 'hello world ' + count;
+        msg2.data = 'hello world2 ' + count;
         // Publish over ROS
         pub.publish(msg);
+        pub2.publish(msg2);
         // Log through stdout and /rosout
-        rosnodejs.log.info('I said: [' + msg.data + ']');
+        //rosnodejs.log.info('I said: [' + msg.data + ']');
         ++count;
       }, 100);
     });
