43c0ef15e533b4bf04481c261c64d0d2a4dcac23
diprasetyamr <diprasetya.mr@mailbox.org>
2018-08-05 12:48:17 +0200

    add rosnode function

--- app.js
+++ app.js
@@ -20,6 +20,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+function talker() {
+  // Register node with ROS master
+  rosnodejs.initNode('/talker_node')
+    .then((rosNode) => {
+      // Create ROS publisher on the 'chatter' topic with String message
+      let pub = rosNode.advertise('/chatter', std_msgs.String);
+      let count = 0;
+      const msg = new std_msgs.String();
+      // Define a function to execute every 100ms
+      setInterval(() => {
+        // Construct the message
+        msg.data = 'hello world ' + count;
+        // Publish over ROS
+        pub.publish(msg);
+        // Log through stdout and /rosout
+        rosnodejs.log.info('I said: [' + msg.data + ']');
+        ++count;
+      }, 100);
+    });
+}
+
+talker();
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
