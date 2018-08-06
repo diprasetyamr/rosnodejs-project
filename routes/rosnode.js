8bd75b703168922b1f6d561c96bfd8446df235ed
diprasetyamr <diprasetya.mr@mailbox.org>
2018-08-06 18:45:05 +0200

    test rosnodejs

--- routes/rosnode.js
+++ routes/rosnode.js
@@ -3,21 +3,16 @@ var router = express.Router();
 var rosnodejs = require('rosnodejs');
 
 const std_msgs = rosnodejs.require('std_msgs').msg;
-const mobile_robot = rosnodejs.require('mobile_robot').msg;
+//const mobile_robot = rosnodejs.require('mobile_robot').msg;
+//const motor = rosnodejs.require('motor').msg;
+const geometry_msgs = rosnodejs.require('geometry_msgs').msg;
 
 function talker() {
   // Register node with ROS master
   rosnodejs.initNode('/nodejs_node')
     .then((rosNode) => {
-      // Create ROS publisher on the 'chatter' topic with String message
-      let pub = rosNode.advertise('/chatter', std_msgs.String);
-      let pub2 = rosNode.advertise('/chatter2', std_msgs.String);
-      let count = 0;
-      const msg = new std_msgs.String();
-      const msg2 = new std_msgs.String();
-      const msg3 = new mobile_robot.velo();
-      const msg4 = new mobile_robot.paths();
 
+      //let pub = rosNode.advertise('/chatter', std_msgs.String);
       let sub = rosNode.subscribe('/chatter', std_msgs.String,
         (data) => { // define callback execution
           rosnodejs.log.info('I heard: [' + data.data + ']');
@@ -29,18 +24,98 @@ function talker() {
           rosnodejs.log.info('I heard2: [' + data.data + ']');
         }
       );
+      // //Location of the robot
+      // let robotLocation = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //     rosnodejs.log.info('Y: [' + data.pose.position.y.toString() + ']');
+      //   }
+      // );
+      //
+      // let aSpeedM1 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let aSpeedM2 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let aSpeedM3 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let aSpeedM4 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tSpeedM1 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tSpeedM2 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tSpeedM3 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tSpeedM4 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tM1 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tM2 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tM3 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+      //
+      // let tM4 = rosNode.subscribe('/slam_out_pose', geometry_msgs.PoseStamped,
+      //   (data) => { // define callback execution
+      //     rosnodejs.log.info('X: [' + data.pose.position.x.toString() + ']');
+      //   }
+      // );
+
       // Define a function to execute every 100ms
-      setInterval(() => {
-        // Construct the message
-        msg.data = 'hello world ' + count;
-        msg2.data = 'hello world2 ' + count;
-        // Publish over ROS
-        pub.publish(msg);
-        pub2.publish(msg2);
-        // Log through stdout and /rosout
-        //rosnodejs.log.info('I said: [' + msg.data + ']');
-        ++count;
-      }, 100);
+      // setInterval(() => {
+      //   // Construct the message
+      //   msg.data = 'hello world ' + count;
+      //   msg2.data = 'hello world2 ' + count;
+      //   // Publish over ROS
+      //   pub.publish(msg);
+      //   pub2.publish(msg2);
+      //   // Log through stdout and /rosout
+      //   //rosnodejs.log.info('I said: [' + msg.data + ']');
+      //   ++count;
+      // }, 100);
     });
 }
 
